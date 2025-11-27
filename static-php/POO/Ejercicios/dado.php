e832e9def8b9c73c7328d89cd60d3088e843f720
SamirItaca <0260scabrera@e-itaca.es>
2025-11-27 17:21:16 +0100

    ejercicios POO

--- static-php/POO/Ejercicios/dado.php
+++ static-php/POO/Ejercicios/dado.php
@@ -7,6 +7,8 @@
         public function __construct() { }
 
         public function tirar() {
+            $resultado = rand($this->min, $this->max);
+            echo "El resultado de la tirado de dado es: " . $resultado . "<br>";
             return rand($this->min, $this->max);
         } 
     }
